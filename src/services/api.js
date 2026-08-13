22afddf22df983c85c70b695209a16c5d93278a3
Osanyindoro <osanyindoro@gmail.com>
2026-08-14 00:01:46 +0100

    Fix syntax error on line 128 of api.js

--- src/services/api.js
+++ src/services/api.js
@@ -125,6 +125,9 @@ export const apiService = {
         localStorage.setItem('mently_user', JSON.stringify(authPayload.user));
         localStorage.setItem('mently_auth_token', authPayload.token);
 
+        resolve(authPayload);
+      }, 500);
+    });
   },
 
   // Register Account API Layer (Associate or Mentor)
